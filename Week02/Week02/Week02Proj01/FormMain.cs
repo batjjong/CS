308f8941ce9e946c5dd28952e0f30199ebe8a3f8
batjjong <42244146+batjjong@users.noreply.github.com>
2025-03-19 12:35:36 +0900

    Update FormMain.cs

--- Week02/Week02/Week02Proj01/FormMain.cs
+++ Week02/Week02/Week02Proj01/FormMain.cs
@@ -1,142 +1,170 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Week02Proj01
+namespace Week3
 {
-    public partial class FormMain : Form
+    public partial class FormWeek3: Form
     {
-        public FormMain()
+        public FormWeek3()
         {
             InitializeComponent();
         }
 
-        private void btnOutput01_Click(object sender, EventArgs e)
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            bool isToggle = chkToggle.Checked; //true or false
 
-            if (isToggle)
+        }
+
+        private void btnProcess1_Click(object sender, EventArgs e)
+        {
+            //배열의 가장큰 특징: 고정길이
+            //배열 생성시 사용할 길이(length)를 정해놓고 시작해야함
+            TextBox[] arrTbxData = new TextBox[5];
+            arrTbxData[0] = tbxData1;
+            arrTbxData[1] = tbxData2;
+            arrTbxData[2] = tbxData3;
+            arrTbxData[3] = tbxData4;
+            arrTbxData[4] = tbxData5;
+
+            //int[] arrIntData = new int[5];
+            //배열의 길이는 반드시 상수(변하지 않는) 값이 들어가야 한다
+            //배열의 길이는 생성 변하지 않기 때문에
+            //다른 배열의 길이로 사용해도 괜찮다
+            //배열 요소의 값은
+            //struct는 struct의 기본값(int는 0),(float는 0.0),(bool는 false)
+            //class는 null이 기본값
+            int[] arrIntData = new int[arrTbxData.Length];
+
+            for(int i=0;i<arrTbxData.Length; i++){
+                if (arrTbxData[i].Text != null && arrTbxData[i].Text != ""){
+                    arrIntData[i] = int.Parse(arrTbxData[i].Text);
+                }
+                else{
+                    //arrIntData[i] = 0; ->과 동일함 ...?
+                }
+            }
+            int result = 0;
+            if (rbtAdd.Checked){
+                for(int i=0; i < arrIntData.Length; i++)
+                {
+                    result += arrIntData[i];
+                }
+            }
+            else if (rbtSub.Checked){
+                result -= arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    result -= arrIntData[i];
+                }
+            }
+            else if (rbtMul.Checked){
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    result *= arrIntData[i];
+                }
+            }
+            else if (rbtDiv.Checked){
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    if (arrIntData[i] == 0){
+                        arrTbxData[i].Focus();
+                        MessageBox.Show("0은 안됨");
+                        return;
+                    }
+                    result /= arrIntData[i];
+                }
+            }
+            else{
+                MessageBox.Show("연산을 선택하세요");
+                return; //메소드를 즉시 종료 후 호출한 곳으로 이동
+            }
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnProcess2_Click(object sender, EventArgs e)
+        {
+            //배열의 가장큰 특징: 고정길이
+            //배열 생성시 사용할 길이(length)를 정해놓고 시작해야함
+            TextBox[] arrTbxData = new TextBox[5];
+            arrTbxData[0] = tbxData1;
+            arrTbxData[1] = tbxData2;
+            arrTbxData[2] = tbxData3;
+            arrTbxData[3] = tbxData4;
+            arrTbxData[4] = tbxData5;
+
+            //int[] arrIntData = new int[5];
+            //배열의 길이는 반드시 상수(변하지 않는) 값이 들어가야 한다
+            //배열의 길이는 생성 변하지 않기 때문에
+            //다른 배열의 길이로 사용해도 괜찮다
+            //배열 요소의 값은
+            //struct는 struct의 기본값(int는 0),(float는 0.0),(bool는 false)
+            //class는 null이 기본값
+            int[] arrIntData = new int[arrTbxData.Length];
+
+            for (int i = 0; i < arrTbxData.Length; i++)
             {
-                string data1 = tbxInput1.Text;
-                string data2 = tbxInput2.Text;
-                string result = data1 + data2; //문자열연결연산자
-                lblResult.Text = result;
+                if (false == string.IsNullOrEmpty(arrTbxData[i].Text))
+                {
+                    arrIntData[i] = int.Parse(arrTbxData[i].Text);
+                }
+                else
+                {
+                    //arrIntData[i] = 0; ->과 동일함 ...?
+                }
+            }
+            int result = 0;
+            if (rbtAdd.Checked)
+            {
+                for (int i = 0; i < arrIntData.Length; i++)
+                {
+                    result += arrIntData[i];
+                }
+            }
+            else if (rbtSub.Checked)
+            {
+                result -= arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    result -= arrIntData[i];
+                }
+            }
+            else if (rbtMul.Checked)
+            {
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    result *= arrIntData[i];
+                }
+            }
+            else if (rbtDiv.Checked)
+            {
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    if (arrIntData[i] == 0)
+                    {
+                        arrTbxData[i].Focus();
+                        MessageBox.Show("0은 안됨");
+                        return;
+                    }
+                    result /= arrIntData[i];
+                }
             }
             else
             {
-                int data1 = int.Parse(tbxInput1.Text);
-                int data2 = int.Parse(tbxInput2.Text);
-                int result = data1 + data2; //산술연산자
-                lblResult.Text = result.ToString();
+                MessageBox.Show("연산을 선택하세요");
+                return; //메소드를 즉시 종료 후 호출한 곳으로 이동
             }
-        }
-
-        private void btnOutput02_Click(object sender, EventArgs e)
-        {
-            if (chkToggle.Checked == false)
-            {
-                int data1 = int.Parse(tbxInput1.Text);
-                int data2 = int.Parse(tbxInput2.Text);
-                int result = data1 + data2; //산술연산자
-                lblResult.Text = "더하기:" + result.ToString();
-            }
-            else
-            {
-                int data1 = int.Parse(tbxInput1.Text);
-                int data2 = int.Parse(tbxInput2.Text);
-                int result = data1 - data2; //산술연산자
-                lblResult.Text = "빼기:" + result; // 문자열+숫자=>문자열 연결 연산자로 동작
-            }
-        }
-
-        private void btnOutput03_Click(object sender, EventArgs e)
-        {
-            int data1 = int.Parse(tbxInput1.Text);
-            int data2 = int.Parse(tbxInput2.Text);
-
-            if (chkToggle.Checked == false)
-            {
-                int result = data1 + data2; //산술연산자
-                lblResult.Text = string.Format("더하기:{0}", result);
-            }
-            else
-            {
-                int result = data1 - data2; //산술연산자
-                lblResult.Text = $"빼기: {result}";//문자열보간법
-            }
-        }
-
-        private void btnOutput04_Click(object sender, EventArgs e)
-        {
-            double data1 = double.Parse(tbxInput1.Text);
-            double data2 = double.Parse(tbxInput2.Text);
-
-            if (chkToggle.Checked == false)
-            {
-                double result = data1 + data2; //산술연산자
-                lblResult.Text = string.Format("더하기:{0}", result);
-            }
-            else
-            {
-                double result = data1 - data2; //산술연산자
-                lblResult.Text = $"빼기: {result}";//문자열보간법
-            }
-        }
-
-        private void btnOutput05_Click(object sender, EventArgs e)
-        {
-            lblResult.Text = tbxInput1.Text;
-            lblResult.Text += Environment.NewLine; // "\r\n"
-            //lblResult.Text = Environment.NewLine; //문자열임.
-            lblResult.Text += tbxInput1.Text.GetType();
-            //lblResult.Text = tbxInput1.Text.GetType();
-            lblResult.Text += Environment.NewLine;
-
-            lblResult.Text += tbxInput1.Text[0];
-            //lblResult.Text = tbxInput1.Text[0];//char (문자)
-            lblResult.Text += Environment.NewLine;
-            lblResult.Text += tbxInput1.Text[0].GetType();
-
-            lblResult.Text += Environment.NewLine;
-            char test1 = tbxInput1.Text[0];
-            //c언어 : 1바이트 (ascii)
-            //c# : 2바이트 (unicode)
-            byte result1 = (byte)test1;   //1바이트 부호 미지원 정수형
-            sbyte result4 = (sbyte)test1;   //1바이트 부호 지원 정수형
-            short result2 = (short)test1; //2바이트 부호지원 정수형
-            ushort result3 = test1;       //2바이트 부호 미지원 정수형
-            //int, uint, long, ulong ...
-
-            lblResult.Text += $"{test1},{result1},{result2},{result3}";
-
-        }
-
-        private void btnOutput06_Click(object sender, EventArgs e)
-        {
-            //정수 -> 실수 :OK
-            //실수 -> 정수 :처리 필요.
-            //작은 숫자 -> 큰 숫자 : OK
-            //큰 숫자 -> 작은 숫자 : 처리 필요
-            int data1 = int.Parse(tbxInput1.Text);
-            float data2 = (float)double.Parse(tbxInput2.Text);
-            long data3 = long.Parse(tbxInput3.Text);
-            int data4 = (int)data3;
-
-            double result1 = data1 + data2 + data3 + data4;
-            lblResult.Text = result1.ToString();
-
-            lblResult.Text += "\r\n";
-            lblResult.Text += "\n";
-
-            // (int)1.9 + (int)1.6 => 2 
-            long result2 = data1 + (long)data2 + data3 + data4;
-            lblResult.Text += result2.ToString();
-
-            lblResult.Text += "\r\n";
-            lblResult.Text += "\n";
-
-            // (int)(1.9 + 1.6) => 3 
-            long result3 = (long)(data1 + data2 + data3 + data4);
-            lblResult.Text += result3.ToString();
+            lblResult.Text = result.ToString();
         }
     }
 }
